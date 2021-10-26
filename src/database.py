3016a56dfa0cc9ba2d49912fd43819b538ffb2e9
Black-Tomcat <tomcat.frantz@gmail.com>
2021-10-27 08:05:30 +1100

    Add environment variables for database host

--- src/database.py
+++ src/database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/stile"
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@{POSTGRES_HOST}/stile"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
